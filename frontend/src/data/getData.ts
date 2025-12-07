deeca2f3e254d93d9d8e258d73cc35aafa66f1aa
Pete <petr.stepanenko@gmail.com>
2025-12-07 10:10:00 -0600

    feat(frontend): create an exportData method

--- frontend/src/data/getData.ts
+++ frontend/src/data/getData.ts
@@ -12,6 +12,11 @@ function loadLocalStorageData(): BaseNode | null {
   return stored ? parseObject(JSON.parse(stored)) : seedData;
 }
 
+function exportData(): string {
+  const data = localStorage.getItem('taskData');
+  return data ?? JSON.stringify(seedData);
+}
+
 function parseObject(object: any): BaseNode {
   let result: BaseNode | null = null;
 
